f3574c5a5b0f83ab83cbb2e0401f6c6d6a9e03ea
Unknown <nichapat18102541@gmail.com>
2018-02-06 11:56:03 +0700

    เสร็จแล้ว ข้อที 2

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -4,7 +4,26 @@
 using namespace std;
 
 int main() {
+    /*ให้นับตัวอักษร*/
     string str = "There are two needles in this haystack with needles.";
     cout << str << endl;
+    cout << "Length :" << str.length() << endl;
+
+    /*เพิ่มข้อความเข้าไป และให้นับตัวอักษร*/
+    str += "I Want to eat needle.";
+    cout << str << endl;
+    cout << "Length :" << str.length() << endl;
+
+    /*ให้ลบออกครึ่งหนึ่ง*/
+    str.erase(str.length()/2, str.length()/2);
+    cout << str << endl;
+    cout << "Length :" << str.length() << endl;
+
+    if (str.find("needle")< str.length()) {
+        cout << "Found 'needle' " << endl;
+    } else {
+        cout << "Not Found 'needle' " << endl;
+    }
+
     return 0;
 }
